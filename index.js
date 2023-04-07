7fef8b8505d2a6632a2f98ad13f7e38d482c1f0a
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2023-04-07 11:43:51 +1000

    Show/hide screen when mobile menu activated

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 // slide nav-expanded into view
 document.querySelector(".nav-expander").addEventListener("click", () => {
     document.querySelector(".nav-expanded").classList.add("display");
+    document.querySelector(".screen").classList.add("show");
 })
 
 // click on nav-close button
 // slide nav-expanded out of view
 document.querySelector(".nav-close").addEventListener("click", () => {
     document.querySelector(".nav-expanded").classList.remove("display");
+    document.querySelector(".content").classList.remove("opacity");
+    document.querySelector(".screen").classList.remove("show");
 })
