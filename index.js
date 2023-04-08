433d2d16712e53ce50fd28fd47a853a979aee067
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2023-04-08 13:08:49 +1000

    Experiment with different ways to add events

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+let mobileMenu = document.querySelector(".mobile-menu");
+let screen = document.querySelector(".screen");
+let hamburgerMenu = document.querySelector(".nav-expander");
+
 // click on nav-expander button
 // slide mobile-menu into view
-document.querySelector(".nav-expander").addEventListener("click", () => {
-    document.querySelector(".mobile-menu").classList.add("display-menu");
-    document.querySelector(".screen").classList.add("show");
+hamburgerMenu.addEventListener("click", () => {
+    mobileMenu.classList.add("display-menu");
+    //document.getElementsByClassName("mobile-menu").style.transform = "translateX(-100%)";
+    screen.classList.add("show");
 })
 
 // click on nav-close button
 // slide mobile-menu out of view
 document.querySelector(".nav-close").addEventListener("click", () => {
-    document.querySelector(".mobile-menu").classList.remove("display-menu");
-    document.querySelector(".screen").classList.remove("show");
+    screen.classList.remove("show");
+    mobileMenu.classList.remove("display-menu");
 })
